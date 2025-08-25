35b7f9ba40306057ce91f2980e180718f566f146
KaizenG <desiigner4074@gmail.com>
2025-08-25 20:12:47 +0800

    Additional components for Category List, Item Form List, and User From (tired af)

--- src/pages/ManageCategory/ManageCategory.jsx
+++ src/pages/ManageCategory/ManageCategory.jsx
@@ -1,8 +1,19 @@
+import CategoryForm from '../../components/CategoryForm/CategoryForm';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import './ManageCategory.css';
+
 const ManageCategory = () => {
     return (
-        <div>
-            Manage Categories
+        <div className="category-container text-light">
+            <div className="left-column">
+                <CategoryForm />
+            </div>
+
+            <div className="right-column">
+                <CategoryList />
+            </div>
         </div>
+        
     );
 }       
 
--- src/pages/ManageUsers/ManageUsers.jsx
+++ src/pages/ManageUsers/ManageUsers.jsx
@@ -1,7 +1,17 @@
+import UserForm from '../../components/UserForm/UserForm';
+import UsersList from '../../components/UsersList/UsersList';
+import './ManageUsers.css';
+
 const ManageUsers = () => { 
     return ( 
-        <div>
-            Manage Users
+        <div className="users-container text-light">
+            <div className="left-column">
+                <UserForm />
+            </div>
+
+            <div className="right-column">
+                <UsersList />
+            </div>
         </div>
     ); 
 }
