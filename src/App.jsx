92353e9ef42be0d96c61ec2089436c79a8d36b5f
KaizenG <desiigner4074@gmail.com>
2025-08-25 19:02:33 +0800

    added simple links TO pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import MenuBar from "./components/MenuBar/MenuBar";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import ManageCategory from "./pages/ManageCategory/ManageCategory";
+import ManageItems from "./pages/ManageItems/ManageItems";
+import ManageUsers from "./pages/ManageUsers/ManageUsers";
+import Explore from "./pages/Explore/Explore";
 
 const App = () => {
   return (
     <div>
       <MenuBar />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/category" element={<ManageCategory />} />
+        <Route path="/users" element={<ManageUsers />} />
+        <Route path="/items" element={<ManageItems />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   )
 }
