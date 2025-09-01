956ba6024342fb7b6a702c31aec8054bba9f53e5
KaizenG <desiigner4074@gmail.com>
2025-09-01 18:40:01 +0800

    partial login component(kisugo ko)

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,28 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className="div bg-light d-flex align-items-center justify-content-center vh-100 login-background">
+            <div className="card shadow-lg w-100" style={{maxWidth: '480px'}}>
+                <div className="card-body">
+                    <div className="text-center">
+                        <h1 className="card-title">Sign in</h1>
+                        <p className="card-text text-muted">
+                            Sign in below to access your account
+                        </p>
+                    </div>
+
+                    <div className="mt-4">
+                        <form>
+                            <div className="mb-4">
+                                <label htmlFor="email" className="form-label text-muted">
+                                    Email address
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
