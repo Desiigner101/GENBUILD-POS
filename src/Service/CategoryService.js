678eedd4b1b72088d5b5ec001a3dc6900be33ac0
KaizenG <desiigner4074@gmail.com>
2025-09-03 15:12:47 +0800

    add proper Authorization header with Bearer token for category endpoints

--- src/Service/CategoryService.js
+++ src/Service/CategoryService.js
@@ -3,16 +3,16 @@ import axios from "axios";
 
 //api call to add category
 export const addCategory = async (category) => {
-    return await axios.post("http://localhost:8080/api/v1.0/admin/categories", category)
+    return await axios.post("http://localhost:8080/api/v1.0/admin/categories", category, {headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}});
 }
 
 //api call to fetch all categories
 export const fetchCategories = async () => {
-    return await axios.get("http://localhost:8080/api/v1.0/admin/categories");
+    return await axios.get("http://localhost:8080/api/v1.0/admin/categories", {headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}});
 }
 
 //api call to delete category
 export const deleteCategory = async (categoryId) => {
-    return await axios.delete(`http://localhost:8080/api/v1.0/categories/${categoryId}`);
+    return await axios.delete(`http://localhost:8080/api/v1.0/categories/${categoryId}`, {headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}});
 }
 
